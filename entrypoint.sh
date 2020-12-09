415e55a926bd019fa8568b86bb18382739f72633
x80486 <masked.github@mailnull.com>
2020-12-09 11:48:17 -0500

    smartgit flatpak

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ set -o errexit
 
 SMARTGIT_JAVA_HOME="/app/extra/smartgit/jre"
 export SMARTGIT_JAVA_HOME
-TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
-export TMPDIR
 
 exec /app/extra/smartgit/bin/smartgit.sh "$@"

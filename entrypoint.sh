b0dadeb9f6a3afcbc51c584913691510b80b2b24
x80486 <gfd2zs93@duck.com>
2023-12-11 21:16:13 -0500

    Setup config file with include directive in order to read from $XDG_CONFIG_HOME/git/config correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 
-SMARTGIT_JAVA_HOME="/app/extra/smartgit/jre"
-export SMARTGIT_JAVA_HOME
+export SMARTGIT_JAVA_HOME="/app/extra/smartgit/jre"
 
-exec /app/extra/smartgit/bin/smartgit.sh "$@"
+update_system () {
+  local xdg_config_git_directory="${HOME}/.config/git"
+  if [[ -d "${xdg_config_git_directory}" ]] && [[ ! -h "${XDG_CONFIG_HOME}/git" ]]; then
+    ln --symbolic "${xdg_config_git_directory}" "${XDG_CONFIG_HOME}"
+  fi
+}
+
+update_system
+
+exec env /app/extra/smartgit/bin/smartgit.sh "$@"

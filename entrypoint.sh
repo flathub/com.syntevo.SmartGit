72eba849cedc538190a464721bd2de071c3b5b31
x80486 <34633617+x80486@users.noreply.github.com>
2025-11-17 07:06:01 -0500

    Replace bundled extraction scripts and reorganize module installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -o errexit -o nounset -o pipefail
 
-export SMARTGIT_JAVA_HOME="/app/extra/smartgit/jre"
-
-update_system () {
-  local xdg_config_git_directory="${HOME}/.config/git"
-  if [[ -d "${xdg_config_git_directory}" ]] && [[ ! -h "${XDG_CONFIG_HOME}/git" ]]; then
-    ln --symbolic "${xdg_config_git_directory}" "${XDG_CONFIG_HOME}"
+sync_user_git_config () {
+  local user_git_config="${HOME}/.config/git"
+  if [[ -d "${user_git_config}" ]] && [[ ! -h "${XDG_CONFIG_HOME}/git" ]]; then
+    ln -s "${user_git_config}" "${XDG_CONFIG_HOME}"
   fi
 }
 
-update_system
+sync_user_git_config
 
-exec env /app/extra/smartgit/bin/smartgit.sh "$@"
+exec env "/app/extra/bin/smartgit.sh" "$@"
